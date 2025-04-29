c211122428c2d568c4e5ac4ccf07314886fce6ab
Anas Haider Mehmood <165678223+Anas2308@users.noreply.github.com>
2025-04-29 14:31:23 +0200

    Initial commit

---
+++ src/test/java/com/Labyrinth/Labyrinth/LabyrinthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Labyrinth.Labyrinth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabyrinthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
